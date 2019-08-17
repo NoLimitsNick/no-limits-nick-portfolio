0729aa7cfa2eefb456cdfbb0e050f6e1435b9940
Nicolas Hunter <nickthedev95@gmail.com>
2019-08-17 16:10:33 -0400

    Moved static markup for work examples into React components

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work'
 
-console.log('Loaded react-dom')
-
-console.log('I always feel like. Somebodies watching me.')
+const myWork = [
+	{
+		'title' : "Serverless Portfolio",
+		'image' : {
+			'desc': "example screenshot of a project involving code",
+			'src' : "images/portfolio_example1.png",
+			'comment' : `First Project`
+		}
+	},
+	{
+		'title' : "Senior Design RFID Inventory Microservices",
+		'image' : {
+			'desc': "Senior Design Project Architecture",
+			'src' : "images/seniordesign_example2.png",
+			'comment' : `Second Project`
+		}
+	},
+	{
+		'title' : "Pi Embedded SQLite Express API ",
+		'image' : {
+			'desc' : "Express API written for Embedded PI SQLite",
+			'src' : "images/sqlwebform_example3.png",
+			'comment' : `Third Project`
+		}
+	},
+]
+ReactDOM.render(<ExampleWork work = {myWork}/>, document.getElementById('example-work'));
